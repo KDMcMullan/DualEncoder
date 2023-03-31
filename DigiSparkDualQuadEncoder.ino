9f9aef54d4ffdac5a6c7e7bcde4ff8c6afd67b0f
KDMcMullan <ken@mcmullanfamily.org.uk>
2023-03-31 09:41:15 +0100

    Updated to look first at interrupts

--- DigiSparkDualQuadEncoder.ino
+++ DigiSparkDualQuadEncoder.ino
@@ -4,25 +4,30 @@
 #include <Wire.h>
 #include "DigiJoystick.h" // output on joystick buttons for debug!
 
-#define byteB 6 // buttons
+#define byteL 6 // buttons
+#define byteH 7 // buttons
 
 #define ExtAddr 0x20 // all address bits pulled low
 
 // IOCON.BANK = 0 by default, so adddresses are sequential
+// IPOL (0x02 and 0x03) remain at 0x00 (default) as we don't wish to invert teh logic
+// INTCON (0x08 and 0x09) remain at 0x00 (default) as we wish to compare to previous
+// DEFVAL (0x06 and 0x07) remain at 0x00 (default) as compare to previous doesn't care
+
 #define regIODIRA  0x00 // IO direction
 #define regIODIRB  0x01
-#define regIPOLA   0x02 // GPIO polatity (1=invert)
-#define regIPOLB   0x03
 #define regINTENA  0x04 // interrupt on change enable 
 #define regINTENB  0x05
-#define regINTCONA 0x08 // interrupt control (0=last value) 
-#define regINTCONB 0x09
 #define regGPPUA   0x0C // pullup resistors (1=pullup)
 #define regGPPUB   0x0D
+#define regINTFA   0x0E // interrupt flags
+#define regINTFB   0x0F
+#define regINTCAPA 0x10 // capture of GPIO at time of interrupt
+#define regINTCAPB 0x11
 #define regGPIOA   0x12 // GPIO port B value
 #define regGPIOB   0x13
-
-#define regIOCON   0x13 // GPIO port B value
+#define regIOCON   0x0A // IO configuration register
+#define regIOCON   0x0B
 
 #define maskPortB 0b11100111 // bits 4 and 3 are not input
 
@@ -34,6 +39,23 @@ char eBuf[2]; // 16 bit I2C buffer
 //const long InterDelay = 0;   // delay between circles
 char tog = 0; // state of watchdog LED
 
+void setReg(unsigned int DevAddr, byte Reg, byte Val)
+{
+// assumes IOCON.SEQOP is false, so address pointer automatically increments
+  Wire.beginTransmission(DevAddr);  // Start I2C Transmission
+  Wire.write(Reg);
+  Wire.write(Val);
+  Wire.endTransmission();
+}
+
+void ReqByteFrom(unsigned int DevAddr, byte Reg)
+{
+  Wire.beginTransmission(DevAddr);  // Start I2C Transmission
+  Wire.write(Reg);
+  Wire.endTransmission();
+  Wire.requestFrom(ExtAddr, 1);    // Request 1 byte of data
+}
+
 void setup() {
 
   // initialize the ATTiny pins
@@ -42,49 +64,39 @@ void setup() {
   // initialize the MCP23017
   Wire.begin();
 
-  Wire.beginTransmission(ExtAddr);  // Start I2C Transmission
-  Wire.write(regIODIRB);
-  Wire.write(maskPortB); // port B inputs, except B3 and B4
-  Wire.endTransmission();
-  Wire.beginTransmission(ExtAddr);  // Start I2C Transmission
-  Wire.write(regGPPUB);
-  Wire.write(maskPortB); // portB pullups enabled, except B3 and B4
-  Wire.endTransmission();
-  Wire.beginTransmission(ExtAddr);  // Start I2C Transmission
-  Wire.write(regINTENB);
-  Wire.write(maskPortB); // portB interrupts, except B3 and B4
-  Wire.endTransmission();
-  Wire.beginTransmission(ExtAddr);  // Start I2C Transmission
-  Wire.write(regINTCONB);
-  Wire.write(maskPortB); // portB value compared against last
-  Wire.endTransmission();
+  setReg(ExtAddr, regIODIRB, maskPortB); // port B inputs, except B3 and B4
+  setReg(ExtAddr, regGPPUB, maskPortB); // portB pullups enabled, except B3 and B4
+  setReg(ExtAddr, regINTENB, maskPortB); // portB interrupts, except B3 and B4
 
 }
 
 void loop() {
 
-  Wire.beginTransmission(ExtAddr);  // Start I2C Transmission
-  Wire.write(regGPIOB);
-  Wire.endTransmission();
-
-  Wire.requestFrom(ExtAddr, 1);    // Request 1 byte of data
+  ReqByteFrom(ExtAddr, regINTFB);
 
   if (Wire.available() == 1)    // Read requested bytes of data: the ADV value, MSbyte first
   {
-    eBuf[0] = Wire.read(); // MS byte
-//    eBuf[1] = Wire.read(); // LS byte
+    eBuf[0] = Wire.read();
 
-  }
+    if (eBuf[0] != 0) {
 
-  if (eBuf[0] != eBuf[1]) {
-    jBuf[byteB] = eBuf[0]; // move byte into joystick buffer
-    eBuf[1] = eBuf[0];
-    DigiJoystick.setValues(jBuf); // output the joystick buffer if it's been updated
-  }  
+      ReqByteFrom(ExtAddr, regINTCAPB);
 
-  DigiJoystick.delay(500); // delay while keeping joystick active
+      if (Wire.available() == 1)    // Read requested bytes of data: the ADV value, MSbyte first
+      {
+        eBuf[1] = Wire.read();
+
+        jBuf[byteL] = eBuf[0]; // move byte into joystick buffer
+        jBuf[byteH] = eBuf[1]; // move byte into joystick buffer
+        DigiJoystick.setValues(jBuf); // output the joystick buffer if it's been updated
+
+      } // INTCAPB available
+    } // INTFB <> 0
+  } // INTFB available
 
   if (tog == 0) {tog = 1;} else {tog  = 0;}
   digitalWrite(pinLED, tog);
+
+  DigiJoystick.delay(500); // delay while keeping joystick active
 
 }
